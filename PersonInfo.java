268d3328197dfe38e7c4e4cdb0040fb408196ee7
Sarajdin <65377917+Sarajdin@users.noreply.github.com>
2020-05-20 01:41:59 -0500

    Add files via upload

---
+++ PersonInfo.java
@@ -0,0 +1,9 @@
+public class PersonInfo{
+	public static void main(String[] args){
+		String studentName = "Jose";
+		int studentId = 1;
+		System.out.println("Welcome "+studentName+" to Techtorial");
+		System.out.println("Your student Id is "+studentId);
+
+	}
+}
